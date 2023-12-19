a2100eb1285388aa4daa84d1fcf391f20cf6b096
Given <givenchyampulembang01@gmail.com>
2023-12-19 17:27:22 +0900

    Add Metadata

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Layout from "@/layout";
 export default function Main({children}) {
   return (
     <>
-        <Layout>
+        <Layout metaTitle="Home">
           <p>Home</p>
         </Layout>
     </>
--- pages/profile.js
+++ pages/profile.js
@@ -5,7 +5,9 @@ import Layout from "@/layout";
   export default function Profile() {
     return (
       <>
-        <Layout>
+        <Layout 
+        metaTitle="Profile" 
+        metaDescription="Semua informasi ini merupakan seputar profile user">
           <p>
             Profile
           </p>
